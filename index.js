02f46202b3ac66b74bdd5314f1d988c01a8f6a18
kaelzhang <i@kael.me>
2014-04-24 15:44:53 +0800

    updates version; fixes tab indents

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
  * module  loader/assets
  */
 
-var 
-
-DOC = document,
-
-HEAD = DOC.getElementsByTagName('head')[0];
+var DOC = document;
+var HEAD = DOC.getElementsByTagName('head')[0];
 
 
 /**
@@ -17,143 +14,143 @@ HEAD = DOC.getElementsByTagName('head')[0];
 /**
  * @param {string} url
  * @param {function()=} callback
- */    
-exports.css = function(uri, callback){
-    var node = DOC.createElement('link');
-    
-    node.href = uri;
-    node.rel = 'stylesheet';
-    
-    callback && assetOnload.css(node, callback);
-    
-    // insert new CSS in the end of `<head>` to maintain priority
-    HEAD.appendChild(node);
-    
-    return node;
+ */
+exports.css = function(uri, callback) {
+  var node = DOC.createElement('link');
+
+  node.href = uri;
+  node.rel = 'stylesheet';
+
+  callback && assetOnload.css(node, callback);
+
+  // insert new CSS in the end of `<head>` to maintain priority
+  HEAD.appendChild(node);
+
+  return node;
 };
 
 /**
  * @param {string} url
  * @param {function()=} callback
  */
-exports.js = function(uri, callback, options){
-    var node = DOC.createElement('script');
-    options = options || {};
-    
-    node.src = uri;
-    node.async = true;
-    
-    callback && assetOnload.js(node, function() {
-        options.remove && HEAD.removeChild(node);
-        callback.call(this);
-    });
-    
-    // loadSrc.__pending = uri;
-    HEAD.insertBefore(node, HEAD.firstChild);
-    // loadSrc.__pending = NULL;
-    
-    return node;
+exports.js = function(uri, callback, options) {
+  var node = DOC.createElement('script');
+  options = options || {};
+
+  node.src = uri;
+  node.async = true;
+
+  callback && assetOnload.js(node, function() {
+    options.remove && HEAD.removeChild(node);
+    callback.call(this);
+  });
+
+  // loadSrc.__pending = uri;
+  HEAD.insertBefore(node, HEAD.firstChild);
+  // loadSrc.__pending = NULL;
+
+  return node;
 };
-    
+
 /**
  * @param {string} url
  * @param {function()=} callback
  */
-exports.img = function(uri, callback){
-    var node = DOC.createElement('img'),
-        delay = setTimeout;
-        
-    function complete(name){
-        node.onload = node.onabort = node.onerror = complete = NULL;
-        
-        // on IE, `onload` event may be fired during the setting{1} of the src of image node
-        // so setTimeout to make sure the callback function will be executed after the current loadSrc.img stack.
-        setTimeout(function(){
-            callback.call(node, {type: name});
-            node = NULL;
-        }, 0);
+exports.img = function(uri, callback) {
+  var node = DOC.createElement('img'),
+    delay = setTimeout;
+
+  function complete(name) {
+    node.onload = node.onabort = node.onerror = complete = NULL;
+
+    // on IE, `onload` event may be fired during the setting{1} of the src of image node
+    // so setTimeout to make sure the callback function will be executed after the current loadSrc.img stack.
+    setTimeout(function() {
+      callback.call(node, {
+        type: name
+      });
+      node = NULL;
+    }, 0);
+  };
+
+  callback && ['load', 'abort', 'error'].forEach(function(name) {
+    node['on' + name] = function() {
+      complete(name);
     };
+  });
 
-    callback && ['load', 'abort', 'error'].forEach(function(name){
-        node['on' + name] = function(){
-            complete(name);
-        };
-    });
+  // {1}
+  node.src = uri;
 
-    // {1}
-    node.src = uri;
-    
-    callback && node.complete && complete && complete('load');
-    
-    return node;
+  callback && node.complete && complete && complete('load');
+
+  return node;
 };
 
-
-var
 
 // @this {element}
-assetOnload = {
-    js: ( DOC.createElement('script').readyState ?
-    
-        /**
-         * @param {DOMElement} node
-         * @param {!function()} callback asset.js makes sure callback is not null
-         */
-        function(node, callback){
-            node.onreadystatechange = function(){
-                var rs = node.readyState;
-                if (rs === 'loaded' || rs === 'complete'){
-                    node.onreadystatechange = NULL;
-                    
-                    callback.call(this);
-                }
-            };
-        } :
-        
-        function(node, callback){
-            node.addEventListener('load', callback, false);
+var assetOnload = {
+  js: (DOC.createElement('script').readyState ?
+
+    /**
+     * @param {DOMElement} node
+     * @param {!function()} callback asset.js makes sure callback is not null
+     */
+    function(node, callback) {
+      node.onreadystatechange = function() {
+        var rs = node.readyState;
+        if (rs === 'loaded' || rs === 'complete') {
+          node.onreadystatechange = NULL;
+
+          callback.call(this);
         }
-    )
-},
+      };
+    } :
+
+    function(node, callback) {
+      node.addEventListener('load', callback, false);
+    }
+  )
+};
 
 // assert.css from jQuery
-cssOnload = ( DOC.createElement('css').attachEvent ?
-    function(node, callback){
-        node.attachEvent('onload', callback);
-    } :
-    
-    function(node, callback){
-        var is_loaded = false,
-            sheet = node['sheet'];
-            
-        if(sheet){
-            if(K.UA.webkit){
-                is_loaded = true;
-            
-            }else{
-                try {
-                    if(sheet.cssRules) {
-                        is_loaded = true;
-                    }
-                } catch (ex) {
-                    if (ex.name === 'NS_ERROR_DOM_SECURITY_ERR') {
-                        is_loaded = true;
-                    }
-                }
-            }
+var cssOnload = DOC.createElement('css').attachEvent 
+  ? function(node, callback) {
+    node.attachEvent('onload', callback);
+  }
+
+  : function(node, callback) {
+    var is_loaded = false,
+      sheet = node['sheet'];
+
+    if (sheet) {
+      if (K.UA.webkit) {
+        is_loaded = true;
+
+      } else {
+        try {
+          if (sheet.cssRules) {
+            is_loaded = true;
+          }
+        } catch (ex) {
+          if (ex.name === 'NS_ERROR_DOM_SECURITY_ERR') {
+            is_loaded = true;
+          }
         }
-    
-        if (is_loaded) {
-            setTimeout(function(){
-                callback.call(node);
-            }, 0);
-        }else {
-            setTimeout(function(){
-                cssOnload(node, callback);
-            }, 10);
-        }
+      }
     }
-); // end var
+
+    if (is_loaded) {
+      setTimeout(function() {
+        callback.call(node);
+      }, 0);
+    } else {
+      setTimeout(function() {
+        cssOnload(node, callback);
+      }, 10);
+    }
+  }; // end var
+
 
 assetOnload.css = cssOnload;
 
